3d519dc9b3f061bc5e906e35c3c787994b608569
Nick Valdez <nicholastvaldez@gmail.com>
2023-03-16 14:10:04 -0500

    cloned mvp no styling

---
+++ src/components/mysounds/MySoundsSamples.js
@@ -0,0 +1,46 @@
+import { useNavigate } from "react-router-dom"
+import { addToCollection } from "../../managers/samples/Collection"
+import { deleteSample } from "../../managers/samples/MySounds"
+
+export const MySoundsSamples = ({
+  id,
+  fileUrl,
+  fileName,
+  producer,
+  instrument,
+  genre,
+}) => {
+  const navigate = useNavigate()
+
+  return (
+    <section key={`sample--${id}`} className="sample">
+      <h2 className="sample__play-button">
+        <a href={fileUrl}>&gt;</a>
+      </h2>
+      <div className="sample__url">
+        {fileName} by {producer}
+      </div>
+
+      <div className="sample__instrument">{instrument}</div>
+      <div className="sample__genre">{genre}</div>
+      <button
+        className="button"
+        onClick={() => {
+          navigate(`/mysounds/edit/${id}`)
+        }}
+      >
+        E
+      </button>
+      <button
+        className="button"
+        onClick={() =>
+          deleteSample(id).then(() => {
+            window.location.reload()
+          })
+        }
+      >
+        X
+      </button>
+    </section>
+  )
+}
