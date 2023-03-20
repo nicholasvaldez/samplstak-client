755f3716d26fa8c1d0467f1b3845abe90de0386d
Nick Valdez <nicholastvaldez@gmail.com>
2023-03-20 13:31:12 -0500

    collection view sounds working

--- src/components/collection/SampleCollection.js
+++ src/components/collection/SampleCollection.js
@@ -1,4 +1,5 @@
-import { FaPlay } from "react-icons/fa"
+import { useRef, useState } from "react"
+import { FaPlay, FaStop } from "react-icons/fa"
 
 export const SampleCollection = ({
   id,
@@ -8,15 +9,55 @@ export const SampleCollection = ({
   instrument,
   genre,
 }) => {
+  const audioRef = useRef()
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const handleLogoClick = () => {
+    if (!isPlaying) {
+      audioRef.current.currentTime = 0 // reset audio to beginning
+      audioRef.current.play()
+      setIsPlaying(true)
+    } else {
+      audioRef.current.pause()
+      setIsPlaying(false)
+    }
+  }
+  const handleAudioEnded = () => {
+    setIsPlaying(false)
+  }
+
+  const file = "http://localhost:8000" + fileUrl
+
   return (
     <section
       key={`sample--${id}`}
       className="sample font-primary w-[1300px] h-[90px] grid grid-cols-9 gap-4  flex items-center bg-[#1E1B1B] text-white mb-[5px] "
     >
       <h2 className=" col-start-1 text-green flex justify-center text-[25px]">
-        <a href={fileUrl}>
-          <FaPlay className="sample__play-button" />
-        </a>
+        <div>
+          {isPlaying ? (
+            <FaStop
+              className="sample__stop-button"
+              onClick={() => {
+                audioRef.current.pause()
+                setIsPlaying(false)
+              }}
+            />
+          ) : (
+            <FaPlay className="sample__play-button" onClick={handleLogoClick} />
+          )}
+          <audio
+            ref={audioRef}
+            src={file}
+            onEnded={handleAudioEnded}
+            onPlay={() => {
+              setIsPlaying(true)
+            }}
+            onPause={() => {
+              setIsPlaying(false)
+            }}
+          />
+        </div>
       </h2>
       <div className="sample__url col-start-2 col-span-4">{fileName}</div>
 
